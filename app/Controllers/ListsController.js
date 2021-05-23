572c1aac384f6c7ee91c757f6cd63fad8721a2b9
boriswart <boriswart@yahoo.com>
2021-05-23 00:28:33 -0600

    local storage working.

--- app/Controllers/ListsController.js
+++ app/Controllers/ListsController.js
@@ -1,5 +1,8 @@
 import { ProxyState } from "../AppState.js"
-// import { ListsService }
+import { List } from "../Models/List.js"
+import { loadState } from "../Utils/LocalStorage.js"
+import { listsService } from "../Services/ListService.js"
+
 //Private
 
 
@@ -10,12 +13,24 @@ export class ListsController {
     constructor() {
         ProxyState.on("lists", this.drawLists)
         this.drawLists()
+        loadState()
     }
 
-    addList() {
-        console.log("Added a Task List");
+    addList(event) {
+        event.preventDefault()
+        let form = event.target
+        console.log("Adding a List", form.name.value)
+        listsService.createList(form.name.value, form.color.value)
+        form.reset
     }
 
+    removeList(listId) {
+        console.log("Removing the List", listId)
+        let keeperLists = ProxyState.lists.filter(x => x.listId !== listId)
+        ProxyState.lists = keeperLists
+        ProxyState.lists = ProxyState.lists
+        // TODO tremove  tasks with listID
+    }
 
 
     drawLists() {
@@ -24,48 +39,42 @@ export class ListsController {
 
         ProxyState.lists.forEach(i => {
             template += /*html*/`
-            <div class="card col-4 my-4">
+            <div class="card col-4 mt-4">
                <div class="card-body text-center">
                 <h5 class="card-title">${i.name}</h5>
-                <p class="card-text">------------
+                <p class="card-text ">------------
                 </p>
                     <div>
                     `
             ProxyState.tasks.forEach(t => {
                 template += /*html*/`
-                            ${t.listId == i.listId ? '<!--Crazy Hard! -->' : ""}
-                            ${t.listId == i.listId ? '<div class="d-flex sb align-items-center">' : ""}
-
-                            ${t.listId == i.listId ? '<input type="checkbox" class="checkbox" id="donechk" autocomplete="off" onclick="app.TasksController.updateTask(' : ""}
-                            ${t.listId == i.listId ? "'" + t.taskId + "','" + t.done + "'" + ')"' : ""}
-                            ${t.listId != i.listId ? "" : t.done ? 'checked >' : '>'}
-
-                            ${t.listId == i.listId ? '<label class="checkbox" for="donechk"></label>' : ""}
-
-                            ${t.listId == i.listId ? t.name : ""}
-                            ${t.listId == i.listId ? '<i class="fa fa-trash" aria-hidden="true" onclick="app.TasksController.removeTask(' : ""}
-                            ${t.listId == i.listId ? "'" + t.taskId + "'" + ')"></i></div>' : ""}`
+                        ${t.listId == i.listId ? '<div class="d-flex sb align-items-center justify-content-around">' : ""}
+                        ${t.listId == i.listId ? '<input type="checkbox" class="checkbox" id="donechk" autocomplete="off" onclick="app.TasksController.updateTask(' : ""}
+                        ${t.listId == i.listId ? "'" + t.taskId + "','" + t.done + "'" + ')"' : ""}
+                        ${t.listId != i.listId ? "" : t.done ? 'checked >' : '>'}
+                        ${t.listId == i.listId ? '<label class="checkbox" for="donechk"></label>' : ""}
+                        ${t.listId == i.listId ? t.name : ""}
+                        ${t.listId == i.listId ? '<i class="fa fa-trash" aria-hidden="true" onclick="app.TasksController.removeTask(' : ""}
+                        ${t.listId == i.listId ? "'" + t.taskId + "'" + ')"></i></div>' : ""}`
             })
 
             template += /*html*/`
                     </div >
-                <div class="d-flex" >
-                    <form onsubmit="app.TasksController.addTask(event,'${i.listId}')">
-                        <input type="text" class="no-outline" id="newtask" name="name" required>
-                            <label class="text " for="newtask"></label>
-                            <button type="submit" class="btn btn-primary">+</button>
+                    <div class="d-flex" >
+                        <form onsubmit="app.TasksController.addTask(event,'${i.listId}')">
+                            <div class="d-flex sb">    
+                                <input type="text" class="no-outline"  minlength="3" maxlength="50" size="10" id="newtask${i.listId}" name="name" required>
+                                <label class="text " for="newtask"></label>
+                                <button type="submit" class="btn btn-primary">+</button>
+                                <i class="fa fa-trash" aria-hidden="true" onclick="app.ListsController.removeList('${i.listId}')"></i>
+                            </div>
                         </form>
                     </div>
-                </div >
-            </div > `
+            </div >
+        </div > `
         })
         document.getElementById("cards-go-here").innerHTML = template
     }
-
-
-
-
-
 
 
 }
